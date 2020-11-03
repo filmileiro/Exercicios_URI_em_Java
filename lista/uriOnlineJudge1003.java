bf3c78fc6ad60654c5e1ead3968b45309ad8eeda
filmileiro <73729082+filmileiro@users.noreply.github.com>
2020-11-03 00:28:55 -0300

    Exercícios solucuonados
    Da questão 1 até a questão 14.

---
+++ lista/uriOnlineJudge1003.java
@@ -0,0 +1,28 @@
+package processamentoDeDados;
+import java.util.Scanner;
+
+public class uriOnlineJudge1003 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Scanner teclado = new Scanner(System.in);
+		
+		int A, B, SOMA;
+		
+		A = teclado.nextInt();
+		B = teclado.nextInt();
+		
+		SOMA = A + B;
+		
+		System.out.println("SOMA = " + SOMA);
+		
+		
+		
+		teclado.close();
+		
+		
+	}
+
+}
